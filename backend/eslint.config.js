31c775170fb2775c6c1b13fdfe042e5cbd4572b4
Vineeth Vijayan <81630465+vineethvijayan314@users.noreply.github.com>
2025-08-28 12:03:05 +0300

    chore: add eslint-plugin-react to frontend dependencies and create eslint configuration for backend TypeScript files

---
+++ backend/eslint.config.js
@@ -0,0 +1,19 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: tseslint.configs.recommended.rules,
+  },
+];
